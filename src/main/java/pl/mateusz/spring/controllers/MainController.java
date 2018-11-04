4c91ad82f5289a1cc588b902d9b6d15d8181f1dd
Mateusz <t.mateusz9333@gmail.com>
2018-11-04 19:18:30 +0100

    Spring MVC minimum config

---
+++ src/main/java/pl/mateusz/spring/controllers/MainController.java
@@ -0,0 +1,14 @@
+package pl.mateusz.spring.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "hello";
+    }
+}
